89047244bbc8ff4e9868024114236d17b420d977
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2021-10-22 17:40:49 +0200

    Update pov.c

--- example/pov.c
+++ example/pov.c
@@ -44,13 +44,13 @@ int main(int argc, char *argv[]) {
         right = track2 * 36 ;
         
         for ( i = 0; i < 36; i++){
-            cAPA102_Set_Pixel_4byte(i, numberArray[left]);
+            cAPA102_Set_Pixel_4byte(i, numberArray[right]);
             left--;
         }
         //cAPA102_Set_Pixel_RGB(i, 255, 0, 0);
         
         for ( i = 36; i < 72; i++){
-            cAPA102_Set_Pixel_4byte(i, numberArray[right]);
+            cAPA102_Set_Pixel_4byte(i, numberArray[left]);
             right++;
         }
         cAPA102_Refresh();
