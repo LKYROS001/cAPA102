9ba52760b9f62b4a407182125e847c64220644ce
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2021-10-24 18:39:19 +0200

    Update dotstar_image_pov.py

--- dotstar_image_pov.py
+++ dotstar_image_pov.py
@@ -79,8 +79,8 @@ distance = 0.0
 for x in range(180):  # For each column of image
     for y in range(72):  # For each pixel in column
         distance = ratio * (y-36)
-        fx = round((distance * math.cos(math.radians(x)))+128)
-        fy = round((-1 * distance * math.sin(math.radians(y))) + 128)
+        fx = round((distance * math.cos(math.radians(x)))+127)
+        fy = round((-1 * distance * math.sin(math.radians(y))) + 127)
         #print("x point is", x, " and y point is ", y)
         #print("x is", fx, " and y is ", fy)
         r = COLUMN[fx][fy][0] << 16 # Gamma-corrected R
