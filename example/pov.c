0a3df87e7bb73c639b914867a99d56ddee303a0c
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2021-11-05 13:45:07 +0200

    Update pov.c

--- example/pov.c
+++ example/pov.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
    // int bri = 0;
    // int dir = 0;
     int time = 0;
-    cAPA102_Init(72, 0, 0, 5);
+    cAPA102_Init(72, 0, 0, 25);
     
     int y;
    
@@ -15,7 +15,7 @@ int main(int argc, char *argv[]) {
     myFile = fopen("readme.txt", "r");
 
     //read file into array
-    int numberArray[12960];
+    int numberArray[25920];
     //int i;
 
     if (myFile == NULL){
@@ -23,43 +23,28 @@ int main(int argc, char *argv[]) {
         exit (0);
     }
 
-    for (y = 0; y < 12960; y++){
+    for (y = 0; y < 25920; y++){
         fscanf(myFile, "%d,", &numberArray[y] );
     }
     fclose(myFile);
 
-    int track1 = 0;
-    int track2 = 180;
-    int left = 0;
-    int right = 0;
+    int x = 0;
     int i;
     while( time > -1 ){
-        if (track1 == 360){
-            track1 = 0;
+        if (x > 25840){
+            x = 0;
+            printf("done\n");
         }
-        if (track2 == 360){
-            track2 = 0;
+        for ( i = 0; i < 72; i++){
+            cAPA102_Set_Pixel_4byte(i, numberArray[x]);
+            x++;
         }
-        left = track1 * 36 + 36;
-        right = track2 * 36 ;
-        
-        for ( i = 0; i < 36; i++){
-            cAPA102_Set_Pixel_4byte(i, numberArray[left]);
-            left--;
-        }
-        //cAPA102_Set_Pixel_RGB(i, 255, 0, 0);
-        
-        for ( i = 36; i < 72; i++){
-            cAPA102_Set_Pixel_4byte(i, numberArray[right]);
-            right++;
-            
-            
-        }
+
         cAPA102_Refresh();
        // y++;
         //time++;
-       track1++;
-       track2++;
+        //usleep(10000);
+        usleep(0.01);
     }
 
     cAPA102_Clear_All();
