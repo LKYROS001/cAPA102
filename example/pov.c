886daead118e0fad21fc7be47e6680004148bb20
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2021-11-05 14:14:23 +0200

    Update pov.c

--- example/pov.c
+++ example/pov.c
@@ -33,7 +33,6 @@ int main(int argc, char *argv[]) {
     while( 1 ){
         if (x > 25840){
             x = 0;
-            printf("done\n");
         }
         for ( i = 0; i < 72; i++){
             cAPA102_Set_Pixel_4byte(i, numberArray[x]);
