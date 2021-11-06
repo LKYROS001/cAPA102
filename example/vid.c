a24af1d8dd3caa14357041db797f9df63c7e2dea
LKYROS001 <70386625+LKYROS001@users.noreply.github.com>
2021-11-07 01:25:42 +0200

    Update vid.c

--- example/vid.c
+++ example/vid.c
@@ -2,50 +2,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<unistd.h>
+//By Ross Lakey
+//Program used to display video
 
 int main(int argc, char *argv[]) {
-   // int bri = 0;
-   // int dir = 0;
-    int time = 0;
-    cAPA102_Init(72, 0, 0, 25);
+
+    cAPA102_Init(72, 0, 0, 25); //set number of LEDs and brightness
     
     int y;
    
     FILE *myFile;
-    myFile = fopen("vid.txt", "r");
-
-    //read file into array
-    
-    //int i;
+    myFile = fopen("vid.txt", "r");//open file
 
     if (myFile == NULL){
         printf("Error Reading File\n");
         exit (0);
     }
     int size = 0;
-    fscanf(myFile, "%d,", &size);
-    int numberArray[size];
+    fscanf(myFile, "%d,", &size);// get number of RGB values from first value in the text file
+    int numberArray[size]; //give size of array
   
     for (y = 0; y < size; y++){
-        fscanf(myFile, "%d,", &numberArray[y] );
+        fscanf(myFile, "%d,", &numberArray[y] );    //read file into array
     }
     fclose(myFile);
 
     int x = 0;
     int i;
-    while( time > -1 ){
-        if (x > (size-75)){
+    while( 1 ){   //loop through array
+        if (x > (size-1)){ //If array end has been reached, reset
             x = 0;
         }
-        for ( i = 0; i < 72; i++){
+        for ( i = 0; i < 72; i++){ //assign each LED a colour
             cAPA102_Set_Pixel_4byte(i, numberArray[x]);
             x++;
         }
 
-        cAPA102_Refresh();
-       // y++;
-        //time++;
-        usleep(0.01);
+        cAPA102_Refresh();//populate LEDs with assigned colour
+        usleep(1);// Timing delay
     }
 
     cAPA102_Clear_All();
